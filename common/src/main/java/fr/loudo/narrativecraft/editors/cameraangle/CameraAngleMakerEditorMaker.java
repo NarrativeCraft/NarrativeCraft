55957f589fd4848d6ef1f90ada8683ecd970ed12
LOUDO <lucaslelouer@gmail.com>
2026-06-18 10:57:10 +0200

    fix(camera-angle): don't despawn characters when stopping editor

--- common/src/main/java/fr/loudo/narrativecraft/editors/cameraangle/CameraAngleMakerEditorMaker.java
+++ common/src/main/java/fr/loudo/narrativecraft/editors/cameraangle/CameraAngleMakerEditorMaker.java
@@ -49,7 +49,6 @@ import fr.loudo.narrativecraft.recording.actions.MovementAction;
 import fr.loudo.narrativecraft.session.PlayerSession;
 import fr.loudo.narrativecraft.utils.FakePlayer;
 import fr.loudo.narrativecraft.utils.UtilsServer;
-import java.util.*;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
@@ -61,6 +60,8 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.phys.Vec3;
+
+import java.util.*;
 
 public class CameraAngleMakerEditorMaker implements EditorMaker {
 
@@ -151,12 +152,6 @@ public class CameraAngleMakerEditorMaker implements EditorMaker {
             }
         }
         Services.PACKET.sendToPlayer(playerSession.getPlayer(), BiStopEditorMaker.INSTANCE);
-        for (CharacterPlacement placement : cameraAngle.getCharacterPlacements()) {
-            Services.PACKET.sendToPlayer(
-                    playerSession.getPlayer(),
-                    new S2CCharacterStoryAction(
-                            placement.getCharacterStory().getId(), S2CCharacterStoryAction.Action.REMOVE));
-        }
     }
 
     public void spawnEntity(CharacterPlacement characterPlacement) {
