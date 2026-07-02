3cd3c34a30b0b3494d80f5df366decb54bfd3c83
LOUDO <lucaslelouer@gmail.com>
2026-07-02 11:32:28 +0200

    fix: fabric server not launching

--- common/src/main/java/fr/loudo/narrativecraft/mixin/CameraMixin.java
+++ common/src/main/java/fr/loudo/narrativecraft/mixin/CameraMixin.java
@@ -103,6 +103,7 @@ public abstract class CameraMixin {
     }
 
     private void narrativecraft$overrideCamera(Vec3 position, Vec3 rotation) {
+        Minecraft.getInstance().options.hideGui = true;
         this.setPosition(position);
         this.setRotation((float) rotation.y, (float) rotation.x);
         this.rotation.rotateZ((float) Math.toRadians(rotation.z()));
