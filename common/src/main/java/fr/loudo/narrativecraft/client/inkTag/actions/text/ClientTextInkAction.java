fcebd25a009acb06a16464f7af58312604c5461e
LOUDO <lucaslelouer@gmail.com>
2026-07-12 11:36:57 +0200

    feat(text-tag): added shadow, mute and text align type
    (cherry picked from commit 33b6fc58e739c59a54c0ec08d8c05c9dad646acb)

--- common/src/main/java/fr/loudo/narrativecraft/client/inkTag/actions/text/ClientTextInkAction.java
+++ common/src/main/java/fr/loudo/narrativecraft/client/inkTag/actions/text/ClientTextInkAction.java
@@ -97,7 +97,6 @@ public class ClientTextInkAction extends TextInkAction {
         float currentOpacity = computeOpacity(partialTick);
         int adjustedAlpha = (int) (FastColor.ARGB32.alpha(Utils.argb(255, color)) * currentOpacity);
         dialogData.setTextColor(Utils.argb(adjustedAlpha, color));
-        dialogData.setTextAlignment(computeTextAlignment());
 
         Font font = Minecraft.getInstance().font;
         cachedTextDimensions = scrollText.computeTextDimensions(width, font, dialogData);
@@ -126,6 +125,7 @@ public class ClientTextInkAction extends TextInkAction {
             dialogData = new DialogData();
             dialogData.setWidth(width);
             dialogData.setTextColor(Utils.argb(255, color));
+            dialogData.setTextAlignment(textAlignment);
             scrollText = new DialogScrollText(dialogData.getLetterSound(), dialogData.isSoundMuted());
             scrollText.setText(text);
             if (noTyping) scrollText.forceFinish();
@@ -177,6 +177,11 @@ public class ClientTextInkAction extends TextInkAction {
                 existing.dialogData.setWidth(width);
                 isRunning = false;
             }
+            case "text_alignment" -> {
+                existing.textAlignment = textAlignment;
+                existing.dialogData.setTextAlignment(textAlignment);
+                isRunning = false;
+            }
             case "fade" -> {
                 existing.fadeState = fadeState;
                 existing.fadeInSeconds = fadeInSeconds;
@@ -211,6 +216,17 @@ public class ClientTextInkAction extends TextInkAction {
                     monitoredInstance = existing;
                 }
             }
+            case "shadow" -> {
+                existing.shadow = shadow;
+                existing.dialogData.setTextShadow(shadow);
+                isRunning = false;
+            }
+            case "mute" -> {
+                existing.mute = mute;
+                existing.dialogData.setSoundMuted(mute);
+                existing.scrollText.setMutedSound(mute);
+                isRunning = false;
+            }
         }
 
         return blocking ? InkActionResult.block() : InkActionResult.ok();
@@ -223,14 +239,6 @@ public class ClientTextInkAction extends TextInkAction {
             case FADE_IN -> (float) Interpolation.lerp(0.05, opacity, t);
             case STAY -> opacity;
             case FADE_OUT -> (float) Interpolation.lerp(opacity, 0.05, t);
-        };
-    }
-
-    private DialogData.TextAlignment computeTextAlignment() {
-        return switch (position) {
-            case TOP_LEFT, MIDDLE_LEFT, BOTTOM_LEFT -> DialogData.TextAlignment.LEFT;
-            case TOP, MIDDLE, BOTTOM -> DialogData.TextAlignment.CENTER;
-            case TOP_RIGHT, MIDDLE_RIGHT, BOTTOM_RIGHT -> DialogData.TextAlignment.RIGHT;
         };
     }
 
